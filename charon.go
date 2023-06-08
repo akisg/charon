49d45b4997cc6d1a9e126909435a3c07d581f339
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-06-08 00:39:33 -0400

    [Req rate policy] show why it does not work.

--- charon.go
+++ charon.go
@@ -53,7 +53,7 @@ func NewPriceTable(initprice int64, nodeName string, callmap map[string]interfac
 		nodeName:           nodeName,
 		callMap:            callmap,
 		priceTableMap:      sync.Map{},
-		rateLimiting:       true,
+		rateLimiting:       false,
 		loadShedding:       true,
 		pinpointThroughput: true,
 		rateLimiter:        make(chan int64, 1),
@@ -92,12 +92,12 @@ func (pt *PriceTable) GetCount() int64 {
 // decrementCounter decrements the counter by 2x every x milliseconds.
 func (pt *PriceTable) decrementCounter() {
 	for range time.Tick(pt.priceUpdateRate) {
-		pt.Decrement(20)
+		pt.Decrement(21)
 
 		ownPrice_string, _ := pt.priceTableMap.LoadOrStore("ownprice", pt.initprice)
 		ownPrice := ownPrice_string.(int64)
 		if pt.GetCount() > 0 {
-			// ownPrice += 1
+			ownPrice += 1
 		} else if ownPrice > 0 {
 			ownPrice -= 1
 		}
@@ -205,6 +205,10 @@ func (pt *PriceTable) LoadShedding(ctx context.Context, tokens int64, methodName
 
 	logger("[Received Req]:	Total price is %d, ownPrice is %d downstream price is %d\n", totalPrice, ownPrice, downstreamPrice)
 
+	if pt.pinpointThroughput {
+		pt.Increment()
+	}
+
 	if extratoken < 0 {
 		logger("[Received Req]: Request rejected for lack of tokens. ownPrice is %d downstream price is %d\n", ownPrice, downstreamPrice)
 		return 0, InsufficientTokens
@@ -213,9 +217,6 @@ func (pt *PriceTable) LoadShedding(ctx context.Context, tokens int64, methodName
 	// I'm thinking about moving it to a separate go routine, and have it run periodically for better performance.
 	// or maybe run it whenever there's a congestion detected, by latency for example.
 	// t.UpdateOwnPrice(ctx, extratoken < 0, tokens, ownPrice)
-	if pt.pinpointThroughput {
-		pt.Increment()
-	}
 
 	// Take the tokens from the req.
 	var tokenleft int64
